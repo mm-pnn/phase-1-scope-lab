81fcc47ce9f314287787be2606de6d1e5457d0f6
Melanie Penn <melanie.penn17@gmail.com>
2022-09-13 14:09:22 -0500

    commit tree 57ebbf781337cd0b88a65a8669be651ce97dba63

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = `bob`
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    return bestCustomer = `not bob`
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`
+}
+
+const leastFavoriteCustomer = `frank`
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bill'
+}
